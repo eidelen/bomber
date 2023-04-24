00e40ba98378a9b29468acff18fcb0b7c389941a
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-04-24 08:39:48 +0200

    bad experiment

--- find_solver_param.py
+++ find_solver_param.py
@@ -4,15 +4,19 @@ from solver import apply_ppo
 # [512, 256, 128, 64], Gamma = 0.8, relu: -> able to finish game
 # [256, 256, 128, 64], Gamma = 0.8, relu: -> good score, but never finished game
 
+# smart bomber
+# nothing!
+#gammas = [0.70, 0.75, 0.80, 0.85, 0.90, 0.95], nn_models = [[512, 256, 128, 64], [1024, 512, 256, 64]], activations = ["relu"]
+
 def find_params():
-    gammas = [0.80, 0.85, 0.90, 0.95]
-    nn_models = [[512, 256, 128, 64]]
+    gammas = [0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
+    nn_models = [[512, 256, 128, 64], [1024, 512, 256, 64]]
     activations = ["relu"]  # tanh, linear
 
     for nn in nn_models:
         for act in activations:
             for g in gammas:
-                apply_ppo(g, nn, act, "movepenalty")
+                apply_ppo(g, nn, act, "SmartBomber")
 
 if __name__ == '__main__':
     find_params()
